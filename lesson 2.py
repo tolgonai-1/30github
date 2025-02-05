ec1027a7934499c6d6d32d4c8cb5d18d3aeab281
tolgonai-1 <mamytovatolgonai@mail.ru>
2025-02-05 15:17:02 +0600

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,38 @@
+name = input()
+print('Привет,', name)
+
+team_name = input()
+print(team_name, '- чемпион!')
+
+
+
+variables1 = 'I was'
+variables2 = 'born'
+variables3 = 'this way'
+print(variables1)
+print(variables2)
+print(variables3)
+
+
+
+#Напишите программу, которая принимает через стандартный поток ввода (команда input()) три строки, а затем выводит их в той же последовательности, каждую на отдельной строке.
+#Test input:
+#I
+#was born
+#this way
+
+variables1=input()
+variables2=input()
+variables3=input()
+print(variables1)
+print(variables2)
+print(variables3)
+
+#Для вывода данных в обратном порядке
+
+text1 = input()
+text2 = input()
+text3 = input()
+print(text3)
+print(text2)
+print(text1)
